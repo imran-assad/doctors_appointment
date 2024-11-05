602bf25cfb173092122c7850c2f32fecee8a36cc
Imran Asad <imranasad5252@gmail.com>
2024-11-05 20:55:58 +0500

    navbar section completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,21 +8,25 @@ import MyAppointments from "./pages/MyAppointments";
 import MyProfile from "./pages/MyProfile";
 import About from "./pages/About";
 import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <>
-      <div className="mx-4 sm:mx-[10%]">MyAppointments</div>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/doctors" element={<Doctors />}></Route>
-        <Route path="/doctors/:speciality" element={<Doctors />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/my-appointments" element={<MyAppointments />}></Route>
-        <Route path="/appointment/:docId" element={<Appointment />}></Route>
-        <Route path="/my-profile" element={<MyProfile />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
+      <div className="mx-4 sm:mx-[10%]">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/doctors" element={<Doctors />}></Route>
+          <Route path="/doctors/:speciality" element={<Doctors />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/my-appointments" element={<MyAppointments />}></Route>
+          <Route path="/appointment/:docId" element={<Appointment />}></Route>
+          <Route path="/my-profile" element={<MyProfile />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </div>
     </>
   );
 };
