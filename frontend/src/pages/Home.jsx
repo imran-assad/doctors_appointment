054fea6e6b3c6def755ce9fa25b115d774678104
Imran Asad <imranasad5252@gmail.com>
2024-11-06 18:39:46 +0500

    top doctors section completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecialityMenu";
+import TopsDoctor from "../components/TopsDoctor";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <div>
         <Header />
         <SpecialityMenu />
+        <TopsDoctor />
       </div>
     </>
   );
